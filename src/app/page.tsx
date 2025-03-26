2e37a946aa25d2675a4915c31aa77d191c328c66
Matt Welson <matt@digitaltransformation.co.nz>
2025-03-27 11:05:58 +1300

    feat: removing event as it has passed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
 import { Header } from "./_components/header";
 import { Hero } from "./_components/hero";
 import { SignupForm } from "./_components/singup-form";
 import { Toaster } from "@/components/ui/sonner";
-import { Event } from "./_components/event";
 import { EverythingSheWanted } from "./_components/everything-she-wanted";
 
 export default function Home() {
@@ -12,10 +10,6 @@ export default function Home() {
       <Header />
       <div className="grid grid-cols-layout [&>*]:col-start-2 gap-4 max-w-full">
         <Hero />
-      </div>
-
-      <div className="grid grid-cols-layout [&>*]:col-start-2 gap-4 max-w-full">
-        <Event />
       </div>
 
       <div className="grid grid-cols-layout [&>*]:col-start-2 gap-4 max-w-full">
